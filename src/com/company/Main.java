6593ef6cd0f84027d893fedc8b4af0d393f8a5d3
EliadNuriel <eliadnuriel8@gmail.com>
2021-10-26 11:11:58 +0300

    GGGG

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,6 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	    System.out.println("This is my first Java Appliction- change 1");
+	    System.out.println("This is my first Java Appliction- change 5");
     }
 }
